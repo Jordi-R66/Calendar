e64fa1ddc04165a8a2068a38f19473f69b23812a
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-07-13 04:44:49 +0200

    Added the signatures for 8 conversion functions

--- Converters.h
+++ Converters.h
@@ -2,3 +2,18 @@
 
 #include "projTypes.h"
 
+// MATHEMATICAL DAY
+JulianDay TimeToDayFrac(TimeStruct timeStruct);
+TimeStruct DayFracToTime(JulianDay dayFrac);
+
+// GREGORIAN
+JulianDay GregToJD(TimeStruct timeStruct);
+TimeStruct JulianDayToGreg(JulianDay JD);
+
+// JULIAN
+JulianDay JulianToJD(TimeStruct timeStruct);
+TimeStruct JulianDayToJulian(JulianDay JD);
+
+// UNIX TIMESTAMP
+JulianDay UNIXToJD(TimeStamp unix_time);
+TimeStamp JulianDayToUNIX(JulianDay JD);
