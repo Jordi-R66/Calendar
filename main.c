27055eb426fa84cc47aa91c07c6cd2ee9a7b896a
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-07-13 04:46:13 +0200

    Added some test code based on Newton's birth

--- main.c
+++ main.c
@@ -6,4 +6,10 @@ void arg_handler(char* argv[], int argc);
 
 int main(int argc, char* argv[]) {
 
+	JulianDay J2000 = GregToJD((TimeStruct){.GREG_DAY = 4, .GREG_MONTH = 1, .GREG_YEAR = 1643, .HOUR = 0});
+	TimeStruct J2000_J = JulianDayToJulian(J2000);
+
+	printf("%u/%u/%lu @ %u:%u:%u\n", J2000_J.JULIAN_DAY, J2000_J.JULIAN_MONTH, J2000_J.JULIAN_YEAR, J2000_J.HOUR, J2000_J.MINUTE, J2000_J.SECONDS);
+
+	return EXIT_SUCCESS;
 }
