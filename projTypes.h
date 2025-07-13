4a1e77fef174948bba1819a1690cb15ffcb21c7f
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-07-13 04:42:31 +0200

    Created the necessary struct and types

--- projTypes.h
+++ projTypes.h
@@ -1,5 +1,29 @@
 #pragma once
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
 typedef double JulianDay;
+typedef int64_t TimeStamp;
+
+typedef struct {
+	// GREGORIAN CALENDAR
+	int64_t GREG_YEAR;
+	uint8_t GREG_MONTH;
+	uint8_t GREG_DAY;
+
+	// JULIAN CALENDAR
+	int64_t JULIAN_YEAR;
+	uint8_t JULIAN_MONTH;
+	uint8_t JULIAN_DAY;
+
+	// TIME OF THE DAY
+	uint8_t HOUR;
+	uint8_t MINUTE;
+	uint8_t SECONDS;
+
+	// UNIVERSAL
+	TimeStamp TIMESTAMP;
+	JulianDay JD;
+} TimeStruct;
