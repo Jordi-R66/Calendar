51b5c67acc7d2de797a8c95b231f9e2747e01972
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-07-18 19:34:19 +0200

    Added a help message and a function to print it

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 
 #include "Converters.h"
 
-void arg_handler(char* argv[], int argc);
+const char helpMessage[] = "The right syntax for this program is the following :\n\n\tDIFFERENCE :\n\t\tprog -d FORMAT_A TIME_A FORMAT_B TIME_B\n\n\tCONVERSION :\n\t\tprog -c SOURCE_FORMAT TIME (TARGET_FORMAT)\n\nSYNTAX FOR TIME :\nIf the selected format (or calendar) is either \"JD\" either \"UT\" the TIME argument will be an Integer for UT and a double-precision floating point number for JD.\nFor every other calendar TIME is composed of two separate arguments :\n\tThe first one is the date written as DD/MM/YYYY and the other one is the time of the day as hh:mm:ss using a 24hr format\n";
+
+void printHelpMessage() {
+	fprintf(stderr, "%s", helpMessage);
+	exit(EXIT_FAILURE);
+}
 
 int main(int argc, char* argv[]) {
 
