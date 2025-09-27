08e716372b9d7d4951eb896bdb0fdc975447ab50
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-09-27 16:04:38 +0200

    Renamed output binary from a.out to calendar.out for clarity

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 mkdir -p output
 
-gcc -g -O0 -std=c17 -fshort-enums Converters.c main.c -o output/a.out -lm
+gcc -g -O0 -std=c17 -fshort-enums Converters.c main.c -o output/calendar.out -lm
 gcc -g -O0 -std=c17 -fshort-enums Converters.c main.c -S
 
 mv *.s output/
