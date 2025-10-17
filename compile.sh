aa5993109681af2bd9dfdbc9f25073f3c4d36636
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-10-17 10:01:04 +0200

    Optimisation du script de compilation : passage à des options de compilation en mode release avec génération de code assembleur.

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 mkdir -p output
 
-gcc -g -O0 -std=c17 -fshort-enums CalendarIO.c Converters.c main.c -o output/calendar.out -lm
-gcc -g -O0 -std=c17 -fshort-enums CalendarIO.c Converters.c main.c -S
+gcc -s -O3 -masm=intel -std=gnu17 -fshort-enums CalendarIO.c Converters.c main.c -o output/calendar.out -lm
+gcc -s -O3 -masm=intel -std=gnu17 -fshort-enums CalendarIO.c Converters.c main.c -S
 
 mv *.s output/
